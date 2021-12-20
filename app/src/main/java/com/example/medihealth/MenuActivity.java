d51ed465578ca7e4bf6f8528d716c68cfbc0e4bb
Dimas <dimasbagusk03@gmail.com>
2021-12-20 16:39:08 +0700

    Navbar Menu

--- app/src/main/java/com/example/medihealth/MenuActivity.java
+++ app/src/main/java/com/example/medihealth/MenuActivity.java
@@ -1,14 +1,36 @@
 package com.example.medihealth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.Navigation.findNavController;
+import static com.example.medihealth.R.id.fragment;
 
 public class MenuActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
+
+
+
+
 }
