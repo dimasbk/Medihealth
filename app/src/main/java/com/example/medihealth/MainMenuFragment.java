1e150a99f6ee26087b1f50a1b86d1a8ec59930b3
Dimas <dimasbagusk03@gmail.com>
2021-12-21 09:38:52 +0700

    Profil dan Medihealth Application

--- app/src/main/java/com/example/medihealth/MainMenuFragment.java
+++ app/src/main/java/com/example/medihealth/MainMenuFragment.java
@@ -1,12 +1,15 @@
 package com.example.medihealth;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MainMenuFragment extends Fragment {
+    Button btn_medihealth, btn_profile;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +62,18 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        Button btn = view.findViewById(R.id.medihealth_app_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent med = new Intent(getActivity(), MedihealthAppActivity.class);
+                startActivity(med);
+            }
+        });
+        return view;
     }
 }
